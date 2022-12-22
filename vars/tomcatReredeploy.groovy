d5c1bbf1c51eb2a605057557f0bd2c4a39f0e2ab
premchand0908 <118746982+premchand0908@users.noreply.github.com>
2022-12-22 13:42:10 +0530

    Update tomcatReredeploy.groovy

--- vars/tomcatReredeploy.groovy
+++ vars/tomcatReredeploy.groovy
@@ -2,6 +2,6 @@ def name(ip,user,creds){
   sshagent(["${creds}"]){
     sh"scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat9/webapps"
     sh"ssh ${user}@${ip} /opt/tomcat9/bin/shutdown.sh"
-    sh"ssh ${user"@${ip} /opt/tomcat9/bin/startup.sh"
+    sh"ssh ${user}@${ip} /opt/tomcat9/bin/startup.sh"
     }
  }
